bf823bb29cbde38cc5f8bd521478d28bdaed7951
fabrisdev <ezepuntocom@hotmail.com>
2025-12-28 22:43:53 -0300

    Update links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
 	url: "https://fabrisdev.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/docs/",
+	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
@@ -101,7 +101,7 @@ const config: Config = {
 					items: [
 						{
 							label: "Tutorial",
-							to: "/docs/intro",
+							to: "/intro",
 						},
 					],
 				},
