29f19883dde1833bc924968a1b934a8d352225f3
fabrisdev <ezepuntocom@hotmail.com>
2025-12-29 21:04:02 -0300

    Update link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
 				},
 				{ to: "/blog", label: "Blog", position: "left" },
 				{
-					href: "https://github.com/facebook/docusaurus",
+					href: "https://github.com/Fabrisdev/htmv",
 					label: "GitHub",
 					position: "right",
 				},
