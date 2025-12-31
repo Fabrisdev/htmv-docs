9b44673936bcd93de52567975b74f85b4c3ba277
fabrisdev <ezepuntocom@hotmail.com>
2025-12-30 21:25:10 -0300

    Revert "Rename to guide" due to huge errors caused
    This reverts commit 6fbe1aaff47ab6173596e37f98bcd0cc443559f5.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
 					type: "docSidebar",
 					sidebarId: "tutorialSidebar",
 					position: "left",
-					label: "Guide",
+					label: "Docs",
 				},
 				{ to: "/blog", label: "Blog", position: "left" },
 				{
